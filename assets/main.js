7dd894108429e3677fff5ece2e2214e36e763ec0
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2022-08-05 14:08:15 +0200

    feat: add file loading and fix minor issues

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,6 @@ import lz from 'lz-string';
 const editor = ace.edit('editor');
 
 editor.setTheme('ace/theme/monokai');
-editor.session.setMode('ace/mode/markdown');
 
 // eslint-disable-next-line no-undef
 const modes = ace.require('ace/ext/modelist');
@@ -24,6 +23,42 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
+const setDefaultContent = () => {
+    editor.session.setMode('ace/mode/markdown');
+    editor.session.setValue(`# An url powered paste app
+
+### What is that ?
+
+This app store the text right here into the page url (no data is store server side)
+
+### Software and library use to make this app:
+
++ [Ace](https://ace.c9.io), Web code editor.
++ [Bootstrap](https://getbootstrap.com), Css library.
++ [Vite](https://vitejs.dev), Front-end development kit.
++ [LZ-String](https://www.npmjs.com/package/lz-string), LZ compression algorithm use to encode data.
++ [ESLint](https://eslint.org), Code quality checking.
++ [Fira Code](https://github.com/tonsky/FiraCode), Monospace font use in the code editor with ligatures related to programming and math.
++ [Xolonium](https://fontlibrary.org/en/font/xolonium), Font use for the nav bar.
+
+### Want more functionality or juste report issues ?
+
++ [GitHub](https://github.com/AlasDiablo/Paste)
+
+### Changelog
+
++ 1.1.0
+    + Add file loading option
+    + Bad url give the value of the landing page
+    + New open source ! (LGPL-3.0)
++ 1.0.1
+    + Fix syntax selector when opening a url with data
+    + Add a link on the title to go on the landing page
++ 1.0.0
+    + Initial version
+`);
+};
+
 const data = params.d;
 if (data !== null && data !== undefined) {
     try {
@@ -33,8 +68,11 @@ if (data !== null && data !== undefined) {
         editor.session.setMode(header.mode);
         editor.session.setValue(lines.join('\n'));
         modeSelector.value = header.mode;
-        // eslint-disable-next-line no-empty
-    } catch (ignored) {}
+    } catch (error) {
+        setDefaultContent();
+    }
+} else {
+    setDefaultContent();
 }
 
 const saveToUrl = () => {
@@ -63,3 +101,20 @@ editor.session.on('change', () => {
 document.getElementById('copy').onclick = () => {
     navigator.clipboard.writeText(window.location.href).then();
 };
+
+document.getElementById('file-loader').addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        editor.session.setValue(e.target.result);
+    };
+    reader.readAsText(file);
+    editor.session.setMode('ace/mode/text');
+    modeSelector.value = 'ace/mode/text';
+    modes.modes.forEach((mode) => {
+        if (mode.extRe.test(file.name)) {
+            editor.session.setMode(mode.mode);
+            modeSelector.value = mode.mode;
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,31 @@
+# An url powered paste app
+
+### What is that ?
+
+This app store the text right here into the page url (no data is store server side)
+
+### Software and library use to make this app:
+
++ [Ace](https://ace.c9.io), Web code editor.
++ [Bootstrap](https://getbootstrap.com), Css library.
++ [Vite](https://vitejs.dev), Front-end development kit.
++ [LZ-String](https://www.npmjs.com/package/lz-string), LZ compression algorithm use to encode data.
++ [ESLint](https://eslint.org), Code quality checking.
++ [Fira Code](https://github.com/tonsky/FiraCode), Monospace font use in the code editor with ligatures related to programming and math.
++ [Xolonium](https://fontlibrary.org/en/font/xolonium), Font use for the nav bar.
+
+### Want more functionality or juste report issues ?
+
++ [GitHub](https://github.com/AlasDiablo/Paste)
+
+### Changelog
+
++ 1.1.0
+    + Add file loading option
+    + Bad url give the value of the landing page
+    + New open source ! (LGPL-3.0)
++ 1.0.1
+    + Fix syntax selector when opening a url with data
+    + Add a link on the title to go on the landing page
++ 1.0.0
+    + Initial version
