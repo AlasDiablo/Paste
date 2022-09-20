61b6c9df126babc56975cc1b28f7075398a47ce2
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2022-09-20 16:05:46 +0200

    docs: add config documentation and jsdoc to server and client

--- assets/main.js
+++ assets/main.js
@@ -4,36 +4,70 @@ import $ from 'jquery';
 // eslint-disable-next-line import/no-unresolved
 import Readme from '../readme.md?raw';
 
+/**
+ * Load Ace editor
+ */
 // eslint-disable-next-line no-undef
 const editor = ace.edit('editor');
 
+/**
+ * Set Monokai as editor theme
+ */
 editor.setTheme('ace/theme/monokai');
 
+/**
+ * Load evey ace modes
+ */
 // eslint-disable-next-line no-undef
 const modes = ace.require('ace/ext/modelist');
+
+/**
+ * Get mode selector element
+ * @type {HTMLElement}
+ */
 const modeSelector = document.getElementById('mode');
+
+/**
+ * Get status box element
+ * @type {HTMLElement}
+ */
 const status = document.getElementById('status');
+
+/**
+ * Get use https check box
+ * @type {HTMLElement}
+ */
 const useHttps = document.getElementById('use-https');
+
+/**
+ * Set default host name
+ * @type {string}
+ */
 let host = '';
 
+/**
+ * Create mode list and add it to 'modeSelector'
+ * @type {string}
+ */
 let modeSelectorInnerHTML = '';
-
 modes.modes.forEach((mode) => {
     if (mode.mode === 'ace/mode/markdown') modeSelectorInnerHTML += `<option value="${mode.mode}" selected>${mode.caption}</option>`;
     else modeSelectorInnerHTML += `<option value="${mode.mode}">${mode.caption}</option>`;
 });
-
 modeSelector.innerHTML = modeSelectorInnerHTML;
 
-const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-});
-
+/**
+ * Set editor default content
+ */
 const setDefaultContent = () => {
     editor.session.setMode('ace/mode/markdown');
     editor.session.setValue(Readme);
 };
 
+/**
+ * Set an encoded content into the editor
+ * @param data{string} an encoded string containing the mode use and the editor content
+ */
 const setContent = (data) => {
     const uncompressedData = lz.decompressFromEncodedURIComponent(data);
     const lines = uncompressedData.split('\n');
@@ -43,6 +77,68 @@ const setContent = (data) => {
     modeSelector.value = header.mode;
 };
 
+/**
+ * Get the editor content and selected mode then encoded it to be use in url paste or server paste
+ * @returns {{compressedData: string, content: string}}
+ */
+const getCompressedData = () => {
+    const doc = editor.session.getDocument().getAllLines();
+    let content = JSON.stringify({
+        mode: editor.session.$modeId,
+        lastEdit: new Date(),
+    });
+    while (doc.length > 0) {
+        content += `\n${doc.shift()}`;
+    }
+    const compressedData = lz.compressToEncodedURIComponent(content);
+    return {
+        content,
+        compressedData,
+    };
+};
+
+/**
+ * Update webpage url with the newly generated data
+ */
+const saveToUrl = () => {
+    const { content, compressedData } = getCompressedData();
+    window.history.pushState(content, '', `?d=${compressedData}`);
+};
+
+/**
+ * Check the connection to a storage server
+ */
+const checkServer = () => {
+    const url = `${useHttps.checked ? 'https://' : 'http://'}${host}/`;
+    status.innerText = `Checking server[${url}]...`;
+    $.ajax({
+        type: 'GET',
+        crossOrigin: true,
+        url,
+        success: (result) => {
+            status.innerText = `Success, quota left : ${result.quota}`;
+            window.localStorage.setItem('host', host);
+        },
+        error: () => {
+            status.innerText = 'Fail to check server...';
+        },
+    });
+};
+
+/**
+ * Get url parameter
+ * @type {module:url.URLSearchParams}
+ */
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+/**
+ * Check if the page have parameter
+ * - If 'd' is present then decode the value and apply it
+ * - Else, if 'host' and 'id' is present ask the host for the value of id and apply it
+ * - If an error is encounter then apply default content.
+ */
 const data = params.d;
 if (data !== null && data !== undefined) {
     try {
@@ -74,57 +170,31 @@ if (data !== null && data !== undefined) {
     setDefaultContent();
 }
 
-const getCompressedData = () => {
-    const doc = editor.session.getDocument().getAllLines();
-    let content = JSON.stringify({
-        mode: editor.session.$modeId,
-        lastEdit: new Date(),
-    });
-    while (doc.length > 0) {
-        content += `\n${doc.shift()}`;
-    }
-    const compressedData = lz.compressToEncodedURIComponent(content);
-    return {
-        content,
-        compressedData,
-    };
-};
-
-const saveToUrl = () => {
-    const { content, compressedData } = getCompressedData();
-    window.history.pushState(content, '', `?d=${compressedData}`);
-};
-
-const checkServer = () => {
-    const url = `${useHttps.checked ? 'https://' : 'http://'}${host}/`;
-    status.innerText = `Checking server[${url}]...`;
-    $.ajax({
-        type: 'GET',
-        crossOrigin: true,
-        url,
-        success: (result) => {
-            status.innerText = `Success, quota left : ${result.quota}`;
-            window.localStorage.setItem('host', host);
-        },
-        error: () => {
-            status.innerText = 'Fail to check server...';
-        },
-    });
-};
-
+/**
+ * Update editor mode when the mode selector have change
+ */
 modeSelector.onchange = (event) => {
     editor.session.setMode(event.target.value);
     saveToUrl();
 };
 
+/**
+ * Update url when the editor content have change
+ */
 editor.session.on('change', () => {
     saveToUrl();
 });
 
+/**
+ * Add to clipboard when the user click on copy
+ */
 document.getElementById('copy').onclick = () => {
     navigator.clipboard.writeText(window.location.href).then();
 };
 
+/**
+ * Load a user file and try yo found the file mode
+ */
 document.getElementById('file-loader').addEventListener('change', (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -142,6 +212,9 @@ document.getElementById('file-loader').addEventListener('change', (event) => {
     });
 });
 
+/**
+ * Show the server storage panel and try to connect to a host if one have already been defined
+ */
 document.getElementById('share').onclick = () => {
     document.getElementById('share-panel').style.display = 'block';
     const savedHost = window.localStorage.getItem('host');
@@ -152,10 +225,17 @@ document.getElementById('share').onclick = () => {
     }
 };
 
+/**
+ * Close server storage panel
+ */
 document.getElementById('cancel-shared').onclick = () => {
     document.getElementById('share-panel').style.display = 'none';
 };
 
+/**
+ * Create encoded data end send it to the server to get the associated id,
+ * and then copy and close the server storage panel
+ */
 document.getElementById('copy-shared').onclick = () => {
     const sharedUrl = document.getElementById('shared-url');
     const baseUrl = `${useHttps.checked ? 'https://' : 'http://'}${host}`;
@@ -184,10 +264,16 @@ document.getElementById('copy-shared').onclick = () => {
     });
 };
 
+/**
+ * Check the server when protocol have been change
+ */
 useHttps.addEventListener('change', () => {
     if (host !== '') checkServer();
 });
 
+/**
+ * Check the server when the host have been change
+ */
 document.getElementById('server-url').addEventListener('change', (event) => {
     host = event.target.value;
     checkServer();
--- server.js
+++ server.js
@@ -6,31 +6,77 @@ import fs from 'fs';
 import https from 'https';
 import config from './server.config.json' assert { type: "json" };
 
+/**
+ * Database object
+ * @type {Database}
+ */
 const db = new Database('./paste.db');
 
+/**
+ * User quota by moths
+ * @type {number}
+ */
 const quota = config.userQuotaByMoths;
 
+/**
+ * RESTFul API
+ * @type {Express}
+ */
 const app = express();
 
+/**
+ * Do a preflight for cors origin
+ */
 app.options('*', cors());
+/**
+ * Fully enable cors origin
+ */
 app.use(cors());
+/**
+ * Add Post handle - json body
+ */
 app.use(bodyParser.json());
+/**
+ * Add Post handle - urlencoded body
+ */
 app.use(bodyParser.urlencoded({ extended: true }));
+/**
+ * Add Post handle - raw body
+ */
 app.use(bodyParser.raw());
 
+/**
+ * Create database table
+ */
 const createDB = db.prepare('CREATE TABLE if not exists paste (id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, date DATE, data TEXT)');
 createDB.run();
 
+/**
+ * Get user left quota
+ * @param ip{number} User ip
+ * @returns {number} quota left
+ */
 const getIpQuota = (ip) => {
-    const stmt = db.prepare('select count(ip) as count from paste where ip = ?');
-    return quota - stmt.get(ip).count;
+    const date = new Date();
+    date.setMonth(date.getMonth() - 1);
+    const stmt = db.prepare('select count(ip) as count from paste where ip = ? and date > ?');
+    return quota - stmt.get(ip, date.getTime()).count;
 };
 
+/**
+ * Set the default
+ * @returns {{quota: number}} left user quota
+ */
 app.get('/', (req, res) => {
     const leftQuota = getIpQuota(req.ip);
     res.send({ quota: leftQuota });
 });
 
+/**
+ * Save the given paste
+ * @param content{string} Encoded paste data
+ * @returns {{quota: number, id: number}} return new quota and the paste id
+ */
 app.post('/save', (req, res) => {
     const leftQuota = getIpQuota(req.ip);
     if (quota <= 0) {
@@ -54,6 +100,11 @@ app.post('/save', (req, res) => {
     res.send({ quota: quota - 1, id: result.lastInsertRowid });
 });
 
+/**
+ * Load paste content
+ * @param id{number} paste id
+ * @returns {{content: string}|{}} paste content or en empty object
+ */
 app.get('/load', (req, res) => {
     const { id } = req.query;
     if (!id) {
@@ -69,6 +120,9 @@ app.get('/load', (req, res) => {
     res.send({ content: result.data });
 });
 
+/**
+ * Remove outdated paste each x days
+ */
 setInterval(() => {
     const date = new Date();
     date.setMonth(date.getMonth() - config.pasteDuration);
@@ -76,6 +130,9 @@ setInterval(() => {
     stmt.run(date.getTime());
 }, (1000 * 60 * 60 * 24) * config.daysBetweenRemoval);
 
+/**
+ * Create a http or https server
+ */
 if (config.useHttps) {
     https.createServer({
                 key: fs.readFileSync("key.pem"),
